d2a153b663aee12910c0f5cd70d838da53fd31d6
Sudharsan c <sudharsanchinnu24@gmail.com>
2026-05-16 21:44:48 +0530

    Delete Bus directory

--- Bus/conductor/Backend/routes/conductorRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { registerConductor, updateLocation } from '../controllers/conductorController.js'
-
-const router = express.Router()
-
-router.post('/register', registerConductor)
-router.post('/location', updateLocation)
-
-export default router
--- Bus/conductor/Backend/routes/ticketRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { createTicket, getTickets } from '../controllers/ticketController.js'
-
-const router = express.Router()
-
-router.post('/', createTicket)
-router.get('/', getTickets)
-
-export default router
